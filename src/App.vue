c351cb7500bc65dfc2e8f1314e68295303c28b23
Lusaca <kny70dr@gmail.com>
2020-10-18 22:28:09 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="header-main">
+        <img src="./assets/logo.svg" width="200px" class="logo" />
+        <nav>
+          <ul>
+            <router-link to="/">
+              <li>Home</li>
+            </router-link>
+            <router-link to="/">
+              <li>All Posts</li>
+            </router-link>
+            <router-link to="/">
+              <li>Tags</li>
+            </router-link>
+            <router-link to="/">
+              <li>About</li>
+            </router-link>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  margin: 0;
+}
+header {
+  width: 100%;
+  height: 69px;
+  border-bottom: 1px solid #b5bdc4;
+}
+
+.header-main {
+  width: 100%;
+  max-width: 1000px;
+  margin-right: auto;
+  margin-left: auto;
+  height: 69px;
+  display: flex;
+}
+
+.logo {
+  flex: 0 0 auto;
+  width: 200px;
+  margin: 13.5px 10px;
+}
+
+nav {
+  flex: 0 0 auto;
+}
+
+nav ul {
+  margin: 0;
+  display: flex;
+}
+
+nav ul li {
+  list-style: none;
+  flex: 0 0 auto;
+  margin-left: 10px;
+  margin-right: 10px;
+  display: inline;
+}
+
+nav ul li:hover {
+  border-bottom: #B8050B solid 2px;
+  
+}
+
+nav ul a {
+  text-decoration: none;
+  line-height: 69px;
+  color: #000;
 }
 </style>
